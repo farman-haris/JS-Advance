7165c9378ca00e95d3fda319880bf51d5d176f58
Farman Haris <farmanharis66@gmail.com>
2024-05-16 01:01:02 +0500

    16-05-2024

---
+++ 01_Lec.js
@@ -0,0 +1,38 @@
+// ----------- Lec 01
+
+/*
+
+const dataID = 85965
+let accountEmail = "farmanharis66@gmail.com"
+var accountPassword = "123456"
+accountCity = "Sahiwal"
+let accountState;
+
+
+accountEmail = "buffyboy327@gmail.com"
+accountPassword = "789456"
+
+
+console.table([dataID, accountEmail, accountPassword, accountCity, accountState]);
+
+*/
+
+// ----------- Lec 02
+
+//number => 2 to the power of 52
+//bigint
+//String => ""
+// boolean => true/false
+// null => standalone Value
+//Symbol => unique
+
+//Object
+
+// ----------- Lec 03
+let score = "45abc"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuInNumber = Number(score);
+
+console.log(typeof valuInNumber);
