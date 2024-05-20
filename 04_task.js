dec7e33895160bda0421c22b4db49073ff5fb69e
Farman Haris <farmanharis66@gmail.com>
2024-05-21 00:09:09 +0500

    Video 20 Completed

--- 04_task.js
+++ 04_task.js
@@ -1,3 +1,6 @@
+/*
+// ----------- Lec 02
+
 function sayMyName() {
     console.log("F");
     console.log("A");
@@ -9,9 +12,9 @@ function sayMyName() {
 
 // sayMyName()
 
-/*function addTwoNo(No01, No02) {
+function addTwoNo(No01, No02) {
     console.log(No01 + No02);     
-}*/
+}
 
 function addTwoNo(No01, No02) {
     let result = No01 + No02;     
@@ -33,3 +36,33 @@ function loginUserMessage(username){
 
 // console.log(loginUserMessage("Farman"))
 console.log(loginUserMessage())
+
+*/
+
+// ----------- Lec 02
+
+function calculateCartPrice(vsl01, vsl02, ...num1) {
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500));
+
+const user = {
+    username: 'farman',
+    price: 199
+}
+
+function handleObject(anyObj) {
+    console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`);
+}
+
+handleObject(user)
+
+
+const myNewArray = [200, 400, 800]
+
+function returnSecondVvalue(getArrayValue) {
+    return getArrayValue[1]
+}
+
+console.log(returnSecondVvalue(myNewArray));
